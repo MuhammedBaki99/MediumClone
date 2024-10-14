7413029c4a15fbaff246bc4fa2e0a51affafd1ef
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-10-14 20:03:21 +0300

    yorumlar formu oluşturuldu

---
+++ app/commentsform/page.jsx
@@ -0,0 +1,33 @@
+"use client"
+import { useFormState } from "react-dom"
+import { useEffect, useRef } from "react";
+import { commentSave } from "./actions";
+
+export default function Comments({ id }) {
+  const [state, action] = useFormState(commentSave, {
+    message: null,
+    error: null,
+  })
+  const formRef = useRef(null);
+  useEffect(() => {
+    if (state?.message) {
+      formRef.current.reset();
+      console.log(state.message);
+
+    }
+  }, [state]);
+
+
+  return (
+    <>
+
+      <form ref={formRef} action={action}>
+        <label htmlFor="content">Yazı:
+          <input id="content" name="content" type="text" /></label>
+        {state?.errors?.content && <small style={{ color: "red" }}>{state.errors.content}</small>}
+        <input type="hidden" value={id} name="commentId" />
+        <button type="submit">Yayınla</button>
+      </form>
+    </>
+  )
+}
