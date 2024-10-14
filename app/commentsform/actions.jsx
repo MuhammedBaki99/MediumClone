3233d0e533b0c41ef9a7643cd9dd3fe69bd4b4bb
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-10-14 20:03:43 +0300

    yorumlar formu için action dosyası hazrılandı

---
+++ app/commentsform/actions.jsx
@@ -0,0 +1,47 @@
+"use server"
+
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+
+export async function commentSave(prevState, formData) {
+  console.log("test");
+  console.log("test");
+
+  const content = formData.get("content");
+
+  const formObj = Object.fromEntries(formData);
+  const commentId = Number(formObj.commentId);
+  
+console.log(commentId);
+
+  const errors = {
+    content: !formObj.content && "Yazı alanı boş olamaz",
+  };
+  for (const key in errors) {
+    if (Object.prototype.hasOwnProperty.call(errors, key)) {
+      const element = errors[key];
+      if (element) return { errors };
+    }
+  }
+  const supabase = createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+
+
+  if (!user) {
+    redirect("/login");
+  } 
+  const { data, error } = await supabase
+    .from('comments')
+    .insert([
+      { content, user_id: user.id, post_id: commentId },
+    ])
+    .select()
+    .single(); 
+
+  if (error) {
+    console.log(error + "asd");
+  }
+
+  redirect("/");
+}
