cfb4ac7913691671ded910e7771c5b8bc05eb31e
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-10-16 16:21:55 +0300

    commentler ekrana basıldı

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,15 @@
-import { createClient } from "@/utils/supabase/server";
-import Comments from "./commentsform/page";
-import CommentList from "./commentlist/page";
+import { createClient } from "@/utils/supabase/server"; 
+import CommentList from "./commentlist/commentlist";
+import LikeBtnPost from "./likebtn/like";
+import Favorites from "./favorites/favori";
+import Link from "next/link";
+import CommentsForm from "./commentsform/commentsform";
 
 export default async function Home() {
   const supabase = createClient();
   let { data: posts } = await supabase
     .from('posts')
-    .select('*')
-  console.log(posts);
-
+    .select('*') 
   return (
     <div className="postList">
       {
@@ -16,10 +17,13 @@ export default async function Home() {
           <div key={i} className="postItem">
             <h1>{x.title}</h1>
             <p>{x.content}</p>
+            <LikeBtnPost id={x.id} />
+            <Favorites id={x.id} />
             <div className="commentList">
-              <CommentList  id={x.id} />
-              <Comments id={x.id} />
+              <CommentList id={x.id} />
+              <CommentsForm id={x.id} />
             </div>
+            <Link href={"/posts/" + x.id}> Detaya Git</Link>
           </div>
         )) : ""
       }
