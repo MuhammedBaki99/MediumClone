7c0bdef501efb93d69168d15891b6a553d742a90
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-10-16 16:23:58 +0300

    post detay için component oluşturuldu

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,5 +1,13 @@
-export default function PostDetail(){
-  return(
-    <h1>Post</h1>
+import CommentList from "@/app/commentlist/commentlist";
+import CommentsForm from "@/app/commentsform/commentsform";
+
+export default function PostDetail() {
+  return (
+    <>
+
+      <h1>Post</h1>
+      <CommentsForm />
+      <CommentList  />
+    </>
   )
 }
