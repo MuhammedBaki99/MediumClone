1af84d5505ae2ad53a6a3b1c21ff8216bd97ad15
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-10-16 16:22:58 +0300

    favori butonu için component oluşturuldu ve işlevi yapıldı

---
+++ app/favorites/favori.jsx
@@ -0,0 +1,38 @@
+"use client"
+import { useEffect, useState } from "react";
+import { favoritesBtn } from "./actions";
+import { useFormState } from "react-dom"
+import { createClient } from "@/utils/supabase/client";
+
+export default function Favorites({ id }) {
+  const [favori, setFavori] = useState(0);
+
+  const [state, action] = useFormState(favoritesBtn, {
+    message: null,
+    error: null,
+  })
+
+  useEffect(() => {
+    async function getPostLike() {
+      const supabase = createClient();
+      let { data: favorites, error } = await supabase
+        .from('favorites')
+        .select('*')
+        .eq("post_id", id)
+        setFavori(favorites.length)
+    }
+    getPostLike()
+ 
+  }, [favori]);
+
+
+  return (
+    <>
+      <form action={action}>
+        <input type="hidden" value={id} name="commentId" />
+        <button type="submit">Favori {favori}</button>
+      </form>
+    </>
+  )
+}
+ 
