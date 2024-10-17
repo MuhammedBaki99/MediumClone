435ab95560551b2a73e9711aa4476b36c2254c0c
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-10-17 18:11:44 +0300

    post detay sayfası yapıldı ve veriler çekildi

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,13 +1,42 @@
-import CommentList from "@/app/commentlist/commentlist";
-import CommentsForm from "@/app/commentsform/commentsform";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import LikeBtnPost from "@/app/likebtn/like";
+import Favorites from "@/app/favoritesform/favori";
+import { Comment } from "@/app/svgfiles/svg";
+import "./postdetail.css"
+export default async function PostDetail({ params }) {
+  const id = Number(params.id);
+  const supabase = createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+  let { data: posts } = await supabase
+    .from('posts')
+    .select('*')
+    .eq("id", id)
 
-export default function PostDetail() {
+  console.log(posts + "asdasd");
+
+
+
+  let { data: comments } = await supabase
+    .from('comments')
+    .select('*')
   return (
-    <>
-
-      <h1>Post</h1>
-      <CommentsForm />
-      <CommentList  />
-    </>
+    <div className="postList">
+      {
+        posts ? posts.map((x, i) => (
+          <Link href={"/posts/" + x.id}  key={i}>
+            <div className="postItem">
+              <h4> <span>{user?.email[0]}</span> {user?.email}</h4>
+              <h1>{x.title}</h1>
+              <p>{x.content}</p>
+              <div className="postBtns">
+                <LikeBtnPost id={x.id} />
+                <p className="commentsBtn"><Comment />{comments.length}</p>
+                <Favorites id={x.id} />
+              </div>
+            </div></Link>
+        )) : ""
+      }
+    </div>
   )
 }
