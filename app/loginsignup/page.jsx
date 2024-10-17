c35e7fb1661e15b77603df155e3c9353628428db
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-10-17 18:08:36 +0300

    giriş yapma ve kayıt olma işlemleri için ortak bir component oluşturuldu ve tasarımı yapıldı

---
+++ app/loginsignup/page.jsx
@@ -0,0 +1,28 @@
+"use client"
+
+import "./loginsignup.css"
+import { useState } from "react"
+import LoginPage from "../login/login";
+import SignUp from "../signup/signup";
+import { MediumLogo } from "../svgfiles/svg"; 
+
+export default function LoginSignUp() {
+  const [sign, setSign] = useState("login");
+  return (
+    <div className="loginsignup">
+      <div className="bgSquare" style={{
+        transform: `${sign === "login" ? "translateX(0px)" : "translateX(450px)"}`
+      }}>
+      </div>
+      <LoginPage setSign={setSign} sign={sign} />
+      {sign === "login" ? <div className="logindesc">
+        <MediumLogo />
+        <p>"Fikirlerini ve hikayelerini paylaşarak dünyanın dört bir yanındaki insanlara ilham ver. Medium ile yaz, öğren ve büyü!"</p>
+      </div> : <div className="signupdesc">
+        <MediumLogo />
+        <p>"Medium'a katılarak, fikirlerini geniş bir kitleye ulaştır ve kendi hikayeni yazmaya başla. Ücretsiz üye ol ve topluluğun bir parçası ol!"</p>
+      </div>}
+      <SignUp setSign={setSign} sign={sign} />
+    </div>
+  )
+}
