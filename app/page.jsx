5f14ee126f8c9023e46e642ef4f613c4a45d75e4
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-10-17 18:07:44 +0300

    anasayfa da gerekn veriler çekilde ve ekrana basıldı

--- app/page.jsx
+++ app/page.jsx
@@ -1,30 +1,36 @@
-import { createClient } from "@/utils/supabase/server"; 
-import CommentList from "./commentlist/commentlist";
+import { createClient } from "@/utils/supabase/server";
 import LikeBtnPost from "./likebtn/like";
 import Favorites from "./favoritesform/favori";
 import Link from "next/link";
-import CommentsForm from "./commentsform/commentsform";
+import { Comment } from "./svgfiles/svg";
 
 export default async function Home() {
   const supabase = createClient();
+  const { data: { user } } = await supabase.auth.getUser();
   let { data: posts } = await supabase
     .from('posts')
-    .select('*') 
+    .select('*')
+
+  console.log(posts);
+
+  let { data: comments } = await supabase
+    .from('comments')
+    .select('*')
   return (
     <div className="postList">
       {
         posts ? posts.map((x, i) => (
-          <div key={i} className="postItem">
-            <h1>{x.title}</h1>
-            <p>{x.content}</p>
-            <LikeBtnPost id={x.id} />
-            <Favorites id={x.id} />
-            <div className="commentList">
-              <CommentList id={x.id} />
-              <CommentsForm id={x.id} />
-            </div>
-            <Link href={"/posts/" + x.id}> Detaya Git</Link>
-          </div>
+          <Link href={"/posts/" + x.id}>
+            <div key={i} className="postItem">
+              {user?.email}
+              <h1>{x.title}</h1>
+              <p>{x.content}</p>
+              <div className="postBtns">
+                <LikeBtnPost id={x.id} />
+                <p className="commentsBtn"><Comment />{comments.length}</p>
+                <Favorites id={x.id} />
+              </div>
+            </div></Link>
         )) : ""
       }
     </div>
